46c803e3482a9e32ac58b428565cee972c07d6a1
Randil22 <thenularandila2002@gmail.com>
2024-09-04 10:51:50 +0530

    updated

---
+++ frontend/src/compants/vehicaleClass.js
@@ -0,0 +1,22 @@
+//class base approch
+import React from "react";
+
+class vehicale extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default vehicale;
