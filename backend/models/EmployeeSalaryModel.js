a942059213c0d940165586539fde810a645bf4fa
Duvini27 <duvinidulwanya@gmail.com>
2024-09-20 18:59:43 +0530

    add logion propertis

--- backend/models/EmployeeSalaryModel.js
+++ backend/models/EmployeeSalaryModel.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const EmployeeSalarySchema = new schema({
+    //for login
     empName: {
         type: String,
         required: true
     },
+    //for login
     password: {
         type: String,
     },
